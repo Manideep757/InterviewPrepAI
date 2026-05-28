ccd66b8a39db51e3d4f87ffc4712b729bc9c6eec
Manideep757 <gollapellymanideep185@gmail.com>
2026-05-29 01:38:03 +0530

    Update multer.js

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,17 +1,27 @@
 import multer from "multer";
+import fs from "fs";
+
+const uploadPath = "public";
+
+// Create folder if not exists
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function(req, file , cb){
-        cb(null , "public")
+    destination: function (req, file, cb) {
+        cb(null, uploadPath);
     },
-    filename: function(req , file , cb){
-        const filename = Date.now() + "-" + file.originalname;
-        cb(null , filename)
-    }
-})
 
+    filename: function (req, file, cb) {
+        const filename = Date.now() + "-" + file.originalname;
+        cb(null, filename);
+    },
+});
 
 export const upload = multer({
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+    },
 });
